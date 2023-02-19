512a3f04c2b2358183b0d050f074bc45caf375da
kugua233 <1499913789@qq.com>
2023-02-19 10:25:40 +0800

    springboot项目初始化

---
+++ src/main/java/com/example/springbootutilsdaily/SpringBootUtilsDailyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootutilsdaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUtilsDailyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUtilsDailyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootutilsdaily/SpringBootUtilsDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootutilsdaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUtilsDailyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
