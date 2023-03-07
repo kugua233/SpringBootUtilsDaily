75ee26da4a4ea7afbd5c13849f90b43b799b66d5
kugua233 <1499913789@qq.com>
2023-03-07 23:02:38 +0800

    springboot项目分層框架初始化

---
+++ example-model/src/test/java/com/example/example/model/ExampleModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
